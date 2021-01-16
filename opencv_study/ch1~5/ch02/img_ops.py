b4baff214aec77b01f564780841023769ea4e113
chanhwi seok <tjrcksgnl@gmail.com>
2021-01-16 21:22:02 +0900

    ch2

--- opencv_study/ch1~5/ch02/img_ops.py
+++ opencv_study/ch1~5/ch02/img_ops.py
@@ -1,16 +1,23 @@
 ﻿import numpy as np
 import cv2
 
+'''
+#shape : 각 차원 크기. (h,w)or(h,w,3)
+#dtype : 원소의 데이터 타입. 일반적 영상 -> numpy.unit8
+#arr : 생성된 영상(numpy.ndarray)
+
+numpy.empty(shape,dtype=float,... )  임의의값으로 초기화된 배열 생성
+numpy.zero(shape,dtype=float,... ) 0으로 초기화된 배열 생성
+numpy.ones(shape, dypte=none,...) 1로 초기화된 배열 생성
+numpy.full(shape, fill_value, dtype=none,...) fill_value로 초기화된 배열 생성(특정한 숫자값 지정하여 생성) (BGR 순서!) 단순 숫자 넣으면 같은 숫자로 통일
+'''
 
 # 새 영상 생성하기
 
-
-img1 = np.empty((240, 320), dtype=np.uint8)      
- # grayscale image
-img2 = np.zeros((240, 320, 3), dtype=np.uint8)   
- # color image
+img1 = np.empty((240, 320), dtype=np.uint8)   # grayscale image
+img2 = np.zeros((240, 320, 3), dtype=np.uint8)  # color image
 img3 = np.ones((240, 320), dtype=np.uint8) * 255  # dark gray
-img4 = np.full((240, 320, 3), (0, 255, 255), dtype=np.uint8)  # yellow
+img4 = np.full((240, 320, 3), (0, 255, 255), dtype=np.uint8)  # yellow  (BGR 순서!)
 
 cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
@@ -22,9 +29,9 @@ cv2.destroyAllWindows()
 # 영상 복사
 img1 = cv2.imread('HappyFish.jpg')
 
-img2 = img1
-img3 = img1.copy()
-
+img2 = img1  #2는 1과 같은 코드 공유
+img3 = img1.copy() #복사본 아예 생성 (이후 img1 수정하든 상관x)
+img1[:, :] = (0, 255, 255)  
 #img1.fill(255)
 
 cv2.imshow('img1', img1)
@@ -39,7 +46,9 @@ img1 = cv2.imread('HappyFish.jpg')
 img2 = img1[40:120, 30:150]  # numpy.ndarray의 슬라이싱
 img3 = img1[40:120, 30:150].copy()
 
-img2.fill(0)
+#img1[:,:]=(0,255,255)  #이후 img1을 바꾸면 img2도 바뀜
+#img2.fill(0) #영상 특정 부분 처리 할때 사용 (잘라내서 다른것으로 채움.)
+cv2.circle(img2, (50,50), 20, (0,0,255), 2)
 
 cv2.imshow('img1', img1)
 cv2.imshow('img2', img2)
--- opencv_study/ch1~5/ch02/mask_op.py
+++ opencv_study/ch1~5/ch02/mask_op.py
@@ -2,17 +2,36 @@ import sys
 import cv2
 
 
+'''
+ROI : 영상에서 특정 연상을 수행하고자하는 임의의 영역
+마스크 연산 : 특정함수 (copyTo, calcHist, bitwise_or, matchTemplate 등) 에 ROI연산 지원, 마스크 영상을 인자로 함께 전달함.
+마스크 영상 : cv2.CV_8UC1 타입(그레이스케일 영상)
+마스크 영상의 픽셀 값이 0이 아닌 위치에서만 연산 수행.
+-> 보통 마스크 영상으로는 0 or 255로 구성된 이진영상 사용
+'''
+'''
+cv2.copyTo(src, mask, dst=None) -> dst
+src : 입력영상
+mask : 마스크 영상. cv2.CV_8U (numpy.uint8)
+        0이 아닌 픽셀에 대해서만 복사 연산 수행
+dst : 출력 영상.
+    src와 크기&타입 같은 dst 이면 dst를 새로 생성X
+    다르다면 dst 새로생성&연산 후 반환
+'''
 # 마스크 영상을 이용한 영상 합성
 src = cv2.imread('airplane.bmp', cv2.IMREAD_COLOR)
-mask = cv2.imread('mask_plane.bmp', cv2.IMREAD_GRAYSCALE)
+mask = cv2.imread('mask_plane.bmp', cv2.IMREAD_GRAYSCALE) #마스크를 자동으로 생성할수도 있음!
 dst = cv2.imread('field.bmp', cv2.IMREAD_COLOR)
 
-if src is None or mask is None or dst is None:
-    print('Image load failed!')
-    sys.exit()
 
-cv2.copyTo(src, mask, dst)
-# dst[mask > 0] = src[mask > 0]
+if src is None or mask is None or dst is None: #예외처리
+    print('Image load failed!')
+    sys.exit() 
+
+#cv2.copyTo(src, mask, dst)  # src,mask 서로 타입이 같아야함  #src의 mask만 따와서 dst 출력
+
+dst[mask > 0] = src[mask > 0]  #불리언 인덱싱
+#mask>0 인 값을 다 찾음 -> 참조형태로 복사 (dst값도 다 바뀜)
 
 cv2.imshow('src', src)
 cv2.imshow('dst', dst)
@@ -24,7 +43,7 @@ cv2.destroyAllWindows()
 src = cv2.imread('cat.bmp', cv2.IMREAD_COLOR)
 logo = cv2.imread('opencv-logo-white.png', cv2.IMREAD_UNCHANGED)
 
-if src is None or logo is None:
+if src is None or logo is None: #예외처리
     print('Image load failed!')
     sys.exit()
 
