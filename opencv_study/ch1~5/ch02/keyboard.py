e3a1986acb0583daa68882c5586a841be594789d
chanhwi seok <tjrcksgnl@gmail.com>
2021-01-18 21:57:35 +0900

    ch2_7

--- opencv_study/ch1~5/ch02/keyboard.py
+++ opencv_study/ch1~5/ch02/keyboard.py
@@ -2,7 +2,12 @@ import sys
 import numpy as np
 import cv2
 
-
+'''
+cv2.waitKey(delay=None) -> retval
+delay : 밀리초 단위 대기 시간. delay<=0 이면 무한히 기다림. 기본값 0
+retval : 눌린 키 값(ASCII code). 눌리지않으면 -1 
+특수 키코드 27=ESC, 13=ENTER, 9=TAB
+'''
 img = cv2.imread('cat.bmp', cv2.IMREAD_GRAYSCALE)
 
 if img is None:
@@ -13,9 +18,9 @@ cv2.namedWindow('image')
 cv2.imshow('image', img)
 
 while True:
-    keycode = cv2.waitKey()
-    if keycode == ord('i') or keycode == ord('I'):
-        img = ~img
+    keycode = cv2.waitKey()  
+    if keycode == ord('i') or keycode == ord('I'):  # 특정키 입력하면 특수 실행
+        img = ~img #논리연산자 '~':반대로 동작
         cv2.imshow('image', img)
     elif keycode == 27:
         break
